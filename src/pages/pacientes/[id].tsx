46792cc8a448b89266348e6a654e2205380380c8
Marcos Vinicius <marcos_archilla96@outlook.com>
2024-12-21 12:36:03 -0300

    11

--- src/pages/pacientes/[id].tsx
+++ src/pages/pacientes/[id].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import api from "@/services/api";
 
+
 interface Paciente {
     id: number;
     nome: string;
